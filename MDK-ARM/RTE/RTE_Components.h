feffcbfa9a42ef9fdfbea06047ec5b76dd37ef55
Marko Pavlin <marko@pavlin.si>
2016-05-29 23:39:02 +0200

    complete project

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usb-rs485' 
+ * Target:  'usb-rs485' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
